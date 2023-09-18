a8af00513ca161699aa31a8d170270ee25a1ffd2
mayank nagwanshi <maayyaannkk@gmail.com>
2023-09-19 01:14:26 +0530

    feat: setting screen

--- app/src/main/java/in/mayanknagwanshi/cointracker/fragments/SettingsFragment.kt
+++ app/src/main/java/in/mayanknagwanshi/cointracker/fragments/SettingsFragment.kt
@@ -1,7 +1,23 @@
 package `in`.mayanknagwanshi.cointracker.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import `in`.mayanknagwanshi.cointracker.R
+import `in`.mayanknagwanshi.cointracker.databinding.FragmentSearchBinding
+import `in`.mayanknagwanshi.cointracker.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
