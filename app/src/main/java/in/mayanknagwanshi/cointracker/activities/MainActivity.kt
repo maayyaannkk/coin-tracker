406b996cfb6e7132cef09efe2c1fe3162eb26f7b
mayank nagwanshi <maayyaannkk@gmail.com>
2023-09-19 11:42:08 +0530

    fix: add viewpager

--- app/src/main/java/in/mayanknagwanshi/cointracker/activities/MainActivity.kt
+++ app/src/main/java/in/mayanknagwanshi/cointracker/activities/MainActivity.kt
@@ -1,8 +1,13 @@
 package `in`.mayanknagwanshi.cointracker.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import `in`.mayanknagwanshi.cointracker.R
 import `in`.mayanknagwanshi.cointracker.databinding.ActivityMainBinding
 import `in`.mayanknagwanshi.cointracker.fragments.MarketFragment
@@ -10,6 +15,7 @@ import `in`.mayanknagwanshi.cointracker.fragments.NewsFragment
 import `in`.mayanknagwanshi.cointracker.fragments.SearchFragment
 import `in`.mayanknagwanshi.cointracker.fragments.SettingsFragment
 import `in`.mayanknagwanshi.cointracker.fragments.WatchlistFragment
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,45 +27,73 @@ class MainActivity : AppCompatActivity() {
 
         //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
 
+        setupViewPager()
+    }
+
+    private fun setupViewPager() {
+        binding.viewPager.adapter = ViewPagerAdapter(this)
+        binding.viewPager.isEnabled = false
+
         binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.menuItemMarket -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragmentContainerView, MarketFragment())
-                        .commit()
+                    binding.viewPager.currentItem = 0
                     true
                 }
 
                 R.id.menuItemWatchlist -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragmentContainerView, WatchlistFragment())
-                        .commit()
+                    binding.viewPager.currentItem = 1
                     true
                 }
 
                 R.id.menuItemSearch -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragmentContainerView, SearchFragment())
-                        .commit()
+                    binding.viewPager.currentItem = 2
                     true
                 }
 
                 R.id.menuItemNews -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragmentContainerView, NewsFragment())
-                        .commit()
+                    binding.viewPager.currentItem = 3
                     true
                 }
 
                 R.id.menuItemSettings -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragmentContainerView, SettingsFragment())
-                        .commit()
+                    binding.viewPager.currentItem = 4
                     true
                 }
 
                 else -> false
             }
         }
+        binding.viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                when (position) {
+                    0 -> binding.bottomNavigationView.selectedItemId = R.id.menuItemMarket
+                    1 -> binding.bottomNavigationView.selectedItemId = R.id.menuItemWatchlist
+                    2 -> binding.bottomNavigationView.selectedItemId = R.id.menuItemSearch
+                    3 -> binding.bottomNavigationView.selectedItemId = R.id.menuItemNews
+                    4 -> binding.bottomNavigationView.selectedItemId = R.id.menuItemSettings
+                }
+            }
+
+        })
     }
+}
+
+class ViewPagerAdapter(activity: MainActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return 5
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> MarketFragment()
+            1 -> WatchlistFragment()
+            2 -> SearchFragment()
+            3 -> NewsFragment()
+            else -> SettingsFragment()
+        }
+    }
+
 }
